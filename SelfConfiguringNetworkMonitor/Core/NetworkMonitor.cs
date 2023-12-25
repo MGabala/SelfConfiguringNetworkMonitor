4a2d4392e0f70b0ab1fc3ced0478543b508b7c03
Mateusz <kontakt@modern-it.pl>
2023-12-25 12:34:28 +0100

    Adding core controller for application

---
+++ SelfConfiguringNetworkMonitor/Core/NetworkMonitor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using SelfConfiguringNetworkMonitor.Configuration;
+
+namespace SelfConfiguringNetworkMonitor.Core
+{
+    public static class NetworkMonitor
+    {
+        private static NetworkMonitorSettings _networkMonitorSettings = new NetworkMonitorSettings();
+        public static void BootstrapFromConfiguration()
+        {
+            var appsettingsConfiguration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+            appsettingsConfiguration.Bind("NetworkMonitorSettings", _networkMonitorSettings);
+        }
+    }
+}
--- SelfConfiguringNetworkMonitor/Program.cs
+++ SelfConfiguringNetworkMonitor/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.Title = "Self-configuring Network Monitor";
+﻿using SelfConfiguringNetworkMonitor.Core;
+using System.Reflection;
+
+Console.Title = "Self-configuring Network Monitor";
+
+NetworkMonitor.BootstrapFromConfiguration();
+
+//var testService = Type.GetType("SelfConfiguringNetworkMonitor.Services.MailService");
+//var test = Activator.CreateInstance(testService) as MailService;
+//test.SendMail("test", "xd");
