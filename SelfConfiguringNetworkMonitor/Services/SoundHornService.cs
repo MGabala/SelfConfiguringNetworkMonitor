d95f4ef03bfb13672d6d558a228f1baf1c6b4c8a
Mateusz <kontakt@modern-it.pl>
2023-12-25 12:02:27 +0100

    Adding new two basic services

---
+++ SelfConfiguringNetworkMonitor/Services/SoundHornService.cs
@@ -0,0 +1,10 @@
+﻿namespace SelfConfiguringNetworkMonitor.Services
+{
+    public class SoundHornService
+    {
+        public void SoundHorn(string volume)
+        {
+            Console.WriteLine($"Making noise with the volume turned up to {volume}.");
+        }
+    }
+}
